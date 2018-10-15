0a47e77544f5a4a4e0388643be5332db8da184e9
Amandeep Kaur <akd6203@gmail.com>
2018-10-15 18:23:29 +0530

    Create View

---
+++ cbvapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from cbvapp import views
+
+app_name='cbvapp'
+
+urlpatterns = [
+    path('',views.collageList.as_view(),name='list'),
+    path('<int:pk>/',views.collageDetail.as_view(),name='detail'),
+    path('create/',views.CollageCreateView.as_view(),name='create'),
+]
